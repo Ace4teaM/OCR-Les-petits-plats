a589b14031e63b17651aad2feee99887d048d6c4
Thomas Auguey <thomas.auguey@hotmail.com>
2026-01-27 09:14:29 +0100

    affiche les recettes depuis les données

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer/Footer";
 import RecipeCard from "../components/RecipeCard/RecipeCard";
 import SearchBar from "../components/SearchBar/SearchBar";
 import styles from './page.module.css';
+import recipes from '@/private/recipes.json'
 
 export default function Home() {
   return (
@@ -11,12 +12,9 @@ export default function Home() {
       <SearchBar></SearchBar>
       <main>
         <section className={styles.recipes}>
-          <RecipeCard></RecipeCard>
-          <RecipeCard></RecipeCard>
-          <RecipeCard></RecipeCard>
-          <RecipeCard></RecipeCard>
-          <RecipeCard></RecipeCard>
-          <RecipeCard></RecipeCard>
+          {recipes.map((r) => (
+          <RecipeCard key={`recipe-${r.id}`} {...r}></RecipeCard>
+          ))}
         </section>
       </main>
       <Footer></Footer>
--- components/RecipeCard/RecipeCard.jsx
+++ components/RecipeCard/RecipeCard.jsx
@@ -1,36 +1,22 @@
 import styles from './RecipeCard.module.css';
 
-export default function RecipeCard() {
+export default function RecipeCard({image, name, time, description, ingredients}) {
   return (
     <article className={styles.card}>
-      <img src='/recipes/Recette01.jpg' className={styles.banner} alt="recipe image"></img>
-      <span className={styles.badge}>10min</span>
-      <h2>Limonade de coco</h2>
+      <img src={`/recipes/${image}`} className={styles.banner} alt="recipe image"></img>
+      <span className={styles.badge}>{time}min</span>
+      <h2>{name}</h2>
       <div className={styles.content}>
     <h3>RECETTE</h3>
-    <p>Mettre les glaçons à votre goût dans le blender, Ajouter le lait  la crème de coco, le jus de 2 citrons et le sucre ensemble. Mixer jusqu'à obtenir la consistance désirée.</p>
+    <p>{description}</p>
     <h3>INGRÉDIENTS</h3>
     <div className={styles.ingredients}>
-        <span>
-            <div className={styles.ingredient}>Lait de coco</div>
-            <div className={styles.quantite}>400ml</div>
-        </span>
-        <span>
-            <div className={styles.ingredient}>Jus de citron</div>
-            <div className={styles.quantite}>2</div>
-        </span>
-        <span>
-            <div className={styles.ingredient}>Crème de coco</div>
-            <div className={styles.quantite}>4 cuillères</div>
-        </span>
-        <span>
-            <div className={styles.ingredient}>Sucre</div>
-            <div className={styles.quantite}>20g</div>
-        </span>
-        <span>
-            <div className={styles.ingredient}>Glaçons</div>
-            <div className={styles.quantite}>2</div>
-        </span>
+        {ingredients.map((ingredient, index) => (
+            <span key={`ingredient-${index}`}>
+                <div className={styles.ingredient}>{ingredient.ingredient}</div>
+                <div className={styles.quantite}>{ingredient.quantity} {ingredient.unit ?? ""}</div>
+            </span>
+        ))}
     </div>
     </div>
     </article>
